0da7ba70ef746cc1f220dbdd747b54822a36dee6
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-02-10 00:39:30 +0700

    Java

---
+++ JavaCore/Java_Multithreading/Shutdown_Hook/anonymous.java
@@ -0,0 +1,20 @@
+package JavaCore.Java_Multithreading.Shutdown_Hook;
+
+public class anonymous {
+    public static void main(String[] args) throws Exception {
+
+        Runtime r = Runtime.getRuntime();
+
+        r.addShutdownHook(new Thread() {
+            public void run() {
+                System.out.println("shut down hook task completed..");
+            }
+        });
+
+        System.out.println("Now main sleeping... press ctrl+c to exit");
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+        }
+    }
+}
