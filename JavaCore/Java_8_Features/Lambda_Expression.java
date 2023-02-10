83dc44e66aeabbc4ccb54531412521d3fdcad79a
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-02-10 16:19:02 +0700

    Java

--- JavaCore/Java_8_Features/Lambda_Expression.java
+++ JavaCore/Java_8_Features/Lambda_Expression.java
@@ -8,15 +8,43 @@
         và truyền nó như một đối số cho các phương thức khác.
 
 
+
+
+
+    Trong Java, có một số quy tắc và cú pháp cần thiết để sử dụng lambda expression. Đây là một số trong số những quy tắc đó:
+
+        1. Cú pháp:
+                    (parameters) -> expression
+
+        2. Tham số:
+
+            Tham số của một lambda expression có thể là một hoặc nhiều biến.
+            Nếu có nhiều hơn một tham số, chúng phải được ngăn cách bởi dấu phẩy.
+            Các tham số cần có kiểu dữ liệu như bình thường, hoặc có thể sử dụng kiểu dữ liệu var
+            (trong Java 10 trở lên).
+
+        3. Toán tử "->":
+
+            Toán tử này được gọi là toán tử arrow.
+            Nó phân chia lambda expression thành hai phần: tham số và biểu thức.
+
+        4. Biểu thức:
+
+            Biểu thức là phần của lambda expression mà tính giá trị của nó.
+            Nó có thể là một phép tính đơn giản hoặc một câu lệnh.
+
+        5. Sử dụng trong interface:
+
+            Một lambda expression chỉ có thể được sử dụng trong một giao diện có một phương thức trừu tượng.
+            Giao diện có thể được sử dụng để tạo ra một đối tượng động bằng cách sử dụng lambda expression.
+
+
+
+
+
     Tại sao sử dụng biểu thức Lambda:
         + Cung cấp bản thực hiện cho interface Chức năng.
         + Viết ít code hơn.
-
-
-    syntax:
-
-            (input parameters) -> expression
-
 
 
     Lưu ý:
@@ -32,12 +60,32 @@ package JavaCore.Java_8_Features;
 import java.util.Arrays;
 import java.util.List;
 
+interface Drawable {
+    public void draw();
+}
+
 public class Lambda_Expression {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        int max = numbers.stream().reduce(0, (a, b) -> Integer.max(a, b));
+        int width = 10;
 
-        System.out.println("Max number: " + max);
+
+        // sử dụng annonymous class
+        Drawable d = new Drawable() {
+            public void draw() {
+                System.out.println("Drawing with Anonymous Class: " + width);
+            }
+        };
+        d.draw();
+
+
+
+        // sử dụng biểu thức lambda
+        Drawable d2 = () -> {
+            System.out.println("Drawing with Lambda: " + width);
+        };
+        d2.draw();
+
+
     }
 }
