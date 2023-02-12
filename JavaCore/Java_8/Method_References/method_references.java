3bcc8aed5bb382bb30cf2e2680d40ea5628bb462
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-02-12 14:14:36 +0700

    Java Method References

---
+++ JavaCore/Java_8/Method_References/method_references.java
@@ -0,0 +1,29 @@
+/*
+*   Trong Java 8, toán tử dấu 2 chấm (::) là một toán tử mới được thêm vào để hỗ trợ việc tạo
+*   ra một method reference.
+*
+*       + Method reference là một cách để trỏ tới một method tồn tại trong một class hoặc instance.
+
+
+*
+    Trong Java 8, có ba kiểu method references:
+
+    1. Reference tới method static: Khi bạn trỏ tới một method static bằng tên class và tên method.
+        Ví dụ: Math::pow
+
+    2. Reference tới method instance: Khi bạn trỏ tới một method instance của một đối tượng cụ thể.
+        Ví dụ: "hello"::length
+
+    3. Reference tới constructor: Khi bạn trỏ tới một constructor của một class.
+        Ví dụ: Person::new
+
+
+* */
+
+package JavaCore.Java_8.Method_References;
+
+public class method_references {
+
+
+
+}
