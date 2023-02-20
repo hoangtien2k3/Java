d4a3e57d5bbcd4839b29a812a14951e545ea5dbf
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-02-21 00:03:58 +0700

    Generic

---
+++ src/java/JavaCore/Generic/WildcardDemo.java
@@ -0,0 +1,44 @@
+package src.java.JavaCore.Generic;
+
+import java.util.*;
+
+public class WildcardDemo {
+
+    public static void main(String[] args) {
+        List<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        System.out.println("Sum of integers in the list: " + sum(intList));
+
+        List<Double> doubleList = new ArrayList<Double>();
+        doubleList.add(1.5);
+        doubleList.add(2.5);
+        doubleList.add(3.5);
+        System.out.println("Sum of doubles in the list: " + sum(doubleList));
+
+        List<Object> objectList = new ArrayList<Object>();
+        objectList.add(1);
+        objectList.add("two");
+        objectList.add(3.0);
+        System.out.println("List of objects: ");
+        printList(objectList);
+    }
+
+    // giới hạn kiểu dữ liệu phải là lớp con của một lớp cụ thể.
+    public static double sum(List<? extends Number> list) {
+        double sum = 0;
+        for (Number n : list) {
+            sum += n.doubleValue();
+        }
+        return sum;
+    }
+
+    // giới hạn kiểu dữ liệu phải là lớp cha của một lớp cụ thể.
+    public static void printList(List<? super Integer> list) {
+        for (Object o : list) {
+            System.out.print(o + " ");
+        }
+        System.out.println();
+    }
+}
