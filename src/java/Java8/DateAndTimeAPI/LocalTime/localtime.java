9eea8b36cfe2216797db46eeed7c0c4cd5535949
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-11-01 17:27:58 +0700

    commit message

--- src/java/Java8/DateAndTimeAPI/LocalTime/localtime.java
+++ src/java/Java8/DateAndTimeAPI/LocalTime/localtime.java
@@ -4,13 +4,16 @@ import java.time.LocalTime;
 
 public class localtime {
     public static void main(String[] args) {
+
         // Java LocalTime Example: now()
         LocalTime  time = LocalTime .now();
         System.out.println(time);
 
+
         // Java LocalTime Example: of()
         time = LocalTime.of(10,43,12);
         System.out.println(time);
+
 
         // Java LocalTime Example: minusHours() and minusMinutes()
         LocalTime time1 = LocalTime.of(10,43,12);
@@ -19,11 +22,13 @@ public class localtime {
         LocalTime time3 = time2.minusMinutes(34);
         System.out.println(time3);
 
+
         // Java LocalTime Example: plusHours() and plusMinutes()
         LocalTime time11 = LocalTime.of(10,43,12);
         System.out.println(time1);
         LocalTime time22=time11.plusHours(4);
         LocalTime time33=time22.plusMinutes(18);
         System.out.println(time33);
+
     }
 }
