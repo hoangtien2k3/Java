376a25d2f365cbd54cff7850216109a8837aafb3
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-11-02 08:40:51 +0700

    commit message

--- src/java/Regex/Pattern_Java/CANON_EQ.java
+++ src/java/Regex/Pattern_Java/CANON_EQ.java
@@ -1,20 +1,16 @@
-/*
-*   Pattern.CANON_EQ : Khi cờ này được chỉ định, hai ký tự sẽ được coi là khớp nếu và chỉ nếu,
-*                      các phân tích hợp quy đầy đủ của chúng khớp nhau.
-*
-* */
-
 package src.java.Regex.Pattern_Java;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/*
+ *   Pattern.CANON_EQ : Khi cờ này được chỉ định, hai ký tự sẽ được coi là khớp nếu và chỉ nếu,
+ *                      các phân tích hợp quy đầy đủ của chúng khớp nhau.
+ * */
 public class CANON_EQ {
     public static void main( String args[] ) {
         String regex = "a\u030A";
-        //Compiling the regular expression
         Pattern pattern = Pattern.compile(regex, Pattern.CANON_EQ);
-        //Retrieving the matcher object
         String[] input = {"\u00E5", "a\u0311", "a\u0325", "a\u030A", "a\u1E03", "a\uFB03" };
         for (String ele : input) {
             Matcher matcher = pattern.matcher(ele);
